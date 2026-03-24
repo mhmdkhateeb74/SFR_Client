204e86eb962759628e5af3d67732fd004847ab27
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2026-03-24 20:19:28 +0200

    Client

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import HomePage from './_Feature/homepage/homepage';
+
+function App() {
+ 
+
+  return (
+    <>
+    <HomePage Pagenumber="one"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/_Feature/homepage/homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HomePage(props){
+
+    return(
+        <>
+        
+        <h1>Home Page</h1>
+        <hr/>
+        <h3>Page:{props.Pagenumber}</h3>
+
+        </>
+    )
+
+}
+
+export default HomePage;
